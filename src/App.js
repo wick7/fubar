26c7224284033cce2e7515b0a3481b64383a1bb7
wick7 <wickersham.craig@gmail.com>
2019-07-01 21:25:42 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,141 +1,97 @@
-import React, {Component, useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Person from './Person/Person.js'
+import React, { Component, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Person from "./Person/Person.js";
 
 class App extends Component {
-
   state = {
-    persons:[
-      {id: 'jn32', name: 'Max', age: 45},
-      {id: 'ogk12', name: 'Fran', age: 5},
-      {id: 'mvcl9', name: 'Dan', age: 15}
+    persons: [
+      { id: "jn32", name: "Max", age: 45 },
+      { id: "ogk12", name: "Fran", age: 5 },
+      { id: "mvcl9", name: "Dan", age: 15 }
     ],
     personView: false
-  }
-  switchAgeHandler = () => {
-    this.setState({
-     persons:[
-       {name: 'Max', age: 245},
-       {name: 'Fran', age: 1000},
-       {name: 'Dan', age: 33}
-     ]
-    })   
-   }
+  };
 
-   showPersons = () => {
-    const shown = this.state.personView
+  showPersons = () => {
+    const shown = this.state.personView;
     this.setState({
       personView: !shown
-    })
-   }
+    });
+  };
 
-   switchNameHandler = (event) => {
+  switchNameHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    //To avoid directly mutating state. Spread op, distributes properites into new object
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    //Make new copy of state with spread, similar to line 40
+    const persons = [...this.state.persons];
+
+    //Replace new copy of current state with updated person's name at correct index point
+    persons[personIndex] = person;
+
     this.setState({
-     persons:[
-       {name: event.target.value, age: 245},
-       {name: 'Fran', age: 1000},
-       {name: 'Dan', age: 33}
-     ]
-    })   
-   }
+      persons: persons
+    });
+  };
 
-   deletMe = (item) => {
-     const persons = this.state.persons.slice()
-     console.log(persons)
-      persons.splice(item,1)
-      this.setState({
-        persons: persons
-      })
-   }
+  deletMe = item => {
+    const persons = this.state.persons.slice();
+    console.log(persons);
+    persons.splice(item, 1);
+    this.setState({
+      persons: persons
+    });
+  };
 
-render() {
+  render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
 
-  const style = {
-    backgroundColor: 'white',
-    font: 'inherit',
-    border: '1px solid blue',
-    padding: '8px',
-    cursor: 'pointer'
-  }
+    let persons = null;
 
-  let persons = null;
+    if (this.state.personView) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, i) => {
+            return (
+              <Person
+                key={person.id}
+                name={person.name}
+                age={person.age}
+                changed={event => this.switchNameHandler(event, person.id)}
+                click={() => this.deletMe(i)}
+              />
+            );
+          })}
+        </div>
+      );
+    }
 
-  if(this.state.personView) {
-    persons = (
+    return (
+      <div className="App">
+        <h1>HI, I'm a React App</h1>
+        <button style={style} onClick={this.showPersons}>
+          Show/Hide
+        </button>
 
-     <div>
-       {
-         this.state.persons.map((person, i) => {
-      
-         return <Person 
-          key={person.id}
-          name={person.name}
-          age={person.age}
-          changed={this.switchNameHandler}
-          click={() => this.deletMe(i)}
-          ></Person>
-        
-         })
-       }
-     </div>
+        {persons}
+      </div>
     );
   }
-
-  return (
-    <div className="App">
-     <h1>HI, I'm a React App</h1>
-       <button onClick={this.switchAgeHandler}>Increase Age</button>
-       <button 
-       style={style}
-       onClick={this.showPersons}
-       
-       >Show/Hide</button>         
-       
-       {persons}
-          
-        </div>
-  );
 }
-}
-
 
 export default App;
-
-
-
-
-// const App = props => {
-//   const [ personState, personsState] = useState({
-//     persons:[
-//       {name: 'Max', age: 45},
-//       {name: 'Fran', age: 5},
-//       {name: 'Dan', age: 15}
-//     ]
-
-//   });
-
-
-  
-
-
-//   return (
-//     <div className="App">
-//     <h1>HI, I'm a React App</h1>
-//     <button onClick={this.switchAgeHandler}>Increase Age</button>
-//       <Person 
-//       name={personState.persons[0].name}
-//       age={personState.persons[0].age}
-//       ></Person>
-//       <Person 
-//       name={personState.persons[1].name}
-//       age={personState.persons[1].age}
-//       ></Person>
-//       <Person 
-//       name={personState.persons[2].name}
-//       age={personState.persons[2].age}
-//       ></Person>
-//     </div>
-//   );
-
-// }
